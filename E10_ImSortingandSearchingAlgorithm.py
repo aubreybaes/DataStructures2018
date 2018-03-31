8c19f2ce11eb7cda96bcb5b74850246f8afade22
aubreybaes <35292042+aubreybaes@users.noreply.github.com>
2018-03-31 22:47:46 +0800

    Add files via upload

---
+++ E10_ImSortingandSearchingAlgorithm.py
@@ -0,0 +1,39 @@
+# EXERCISE NO. 9 -  BINARY TREE
+# Mae Aubrey C. Baes
+# github.com/aubreybaes
+# maeaubrey.baes@gmail.com
+#This program checks if a given number from 1-100 is a prime number
+
+
+def bubblesort(nlist):
+    for passnum in range(len(nlist)-1,0,-1):
+        for i in range(passnum):
+            if nlist[i]>nlist[i+1]:
+                temp = nlist[i]
+                nlist[i] = nlist[i+1]
+                nlist[i+1] = temp
+
+def binary_search(arr,val):
+    if len(arr) == 0 or (len(arr)==1 and arr[0]!=val):
+        print "\n",val, "is NOT a Prime Number!"
+        return False
+
+    mid = arr[len(arr)/2]
+    if val == mid:
+        print "\n",val,"is a Prime Number"
+        return True
+    if val < mid: return binary_search(arr[:len(arr)/2],val)
+    if val > mid: return binary_search(arr[len(arr)/2+1:],val)
+
+if __name__ == "__main__":
+    def prime():
+        nlist = [2,5,7,11,3,23,17,19,13,37,43,31,29,47,41,53,67,61,59,97,83,73,71,89,83,79]
+        bubblesort(nlist)
+
+        print "\nYou may enter an integer and check if it's prime number or not!"
+
+        x = input("Enter an integer: ")
+
+        print binary_search(nlist,x)
+        return prime()
+    prime()
